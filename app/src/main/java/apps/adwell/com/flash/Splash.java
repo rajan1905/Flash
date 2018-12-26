17574e7c526c252bc24a45e1120f1c2108991e7a
rajan1905 <rajan_1905@live.com>
2018-12-26 04:28:45 +0000

    Splash Screen

--- app/src/main/java/apps/adwell/com/flash/Splash.java
+++ app/src/main/java/apps/adwell/com/flash/Splash.java
@@ -1,13 +1,29 @@
 package apps.adwell.com.flash;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Splash extends AppCompatActivity {
+    private final int waitTime = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        final long startTime = System.currentTimeMillis();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                if(startTime + waitTime == System.currentTimeMillis()){
+                    Intent gotoMainScreen = new Intent(Splash.this, Main.class);
+                    startActivity(gotoMainScreen);
+                    finish();
+                }
+            }
+        }).start();
+
     }
 }
